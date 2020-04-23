ef7bffc154c5a0d0878674bcf6ffe3d475331a1c
ElnareHasanova <elnarehasanova14@gmail.com>
2020-04-23 20:00:14 +0400

    add time

--- js/dental.js
+++ js/dental.js
@@ -3,4 +3,35 @@ $( function() {
     showOtherMonths: true,
     selectOtherMonths: true
   });
+  
+  $(document).on("click", ".hasDatepicker",function () {
+    $(".date").css("display","block");
+    
+  })
+
+ 
+  // $(document).on("click", ".btn1",function () {
+    
+  // $('input[type="button"]').attr('disabled', true);
+
+  
+  //     var text_value = $('input[name="textField"]').val();
+  //     if ( text_value != '') {
+  //         $('input[type="button"]').attr('disabled', false);
+  //     } else {
+  //         $('input[type="button"]').attr('disabled', true);
+  //     }
+  
+
+      
+     
+   
+    
+  // })
+  $(document).on("click", ".btn1",function () {
+
+    $(".wait").css("display","block");
+    
+  })
+
 } );
